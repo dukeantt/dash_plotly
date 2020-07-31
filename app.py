b60e3498be5c9d1aac803274d8bc0575b130f1b0
DucAnh <ngducanh1223@gmail.com>
2020-07-31 14:37:18 +0700

    Show piechart without error

--- app.py
+++ app.py
@@ -7,17 +7,13 @@ from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 
 import pandas as pd
-
-main_df = pd.DataFrame()
+import plotly.graph_objects as go
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 app.layout = html.Div([
     dcc.Upload(
         id='upload-data',
@@ -38,47 +34,109 @@ app.layout = html.Div([
         # Allow multiple files to be uploaded
         multiple=True
     ),
-
-    dcc.Tabs([
-        dcc.Tab(label='Tab one', children=[
-            dcc.Graph(id='proportion-in-month'),
-        ]),
-        dcc.Tab(label='Tab two', children=[
-            dcc.Graph(id='outcome-proportion-of-all-uc'),
-        ]),
-        dcc.Tab(label='Tab three', children=[
-            dcc.Graph(id='outcome-of-uc'),
-        ]),
-    ]),
+    html.Div(
+        [html.Div(id='first-pie'),
+         html.Div(id='second-pie'), ]
+    ),
+    html.Div(id='third-pie'),
+    html.Div(id='forth-pie'),
 ])
 
 
-def uc_proportion_in_month(labels: list, values: list):
-    fig = go.Figure(data=[go.Pie(labels=labels,
-                                 values=values)])
-    fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=20,
-                      marker=dict(line=dict(color='#000000', width=2)))
-    return fig
+def create_trace1():
+    colors = ['mediumturquoise', 'darkorange', 'lightgreen']
+    trace = go.Pie(
+        labels=['Other', 'UC 1', 'UC 2'],
+        values=[167, 18, 38],
+        hoverinfo='label+percent',
+        textinfo='label+value+percent',
+        textfont_size=15,
+        marker=dict(colors=colors, line=dict(color='#000000', width=2))
+    )
+    first_pie = html.Div(
+        # id="leads_source_container",
+        className="six columns chart_div pretty_container",
+        children=[
+            html.P("First pie"),
+            dcc.Graph(
+                figure={"data": [trace]},
+                style={"height": "90%", "width": "98%"},
+                config=dict(displayModeBar=False),
+            ),
+        ],
+    ),
+    return first_pie
 
 
-def outcome_proportion_of_uc(labels: list, values: list):
-    fig = go.Figure(data=[go.Pie(labels=labels,
-                                 values=values)])
-    fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=20,
-                      marker=dict(line=dict(color='#000000', width=2)))
-    return fig
+def create_trace2():
+
+    trace = go.Pie(
+        labels=['thanks', 'shipping', 'handover', "silence", "other", "agree"],
+        values=[0, 5, 18, 24, 8, 1],
+        direction="clockwise",
+        sort=False,
+        rotation=120,
+        hoverinfo='label+percent',
+        textinfo='label+value',
+        textfont_size=15,
+        marker=dict(line=dict(color='#000000', width=2))
+    )
+    second_pie = html.Div(
+        # id="leads_source_container",
+        className="six columns chart_div pretty_container",
+        children=[
+            html.P("Second pie"),
+            dcc.Graph(
+                figure={"data": [trace]},
+                style={"height": "90%", "width": "98%"},
+                config=dict(displayModeBar=False),
+            ),
+        ],
+    ),
+    return second_pie
 
 
-def outcome_of_uc(labels: list, values1: list, values2: list):
-    fig = make_subplots(1, 2, specs=[[{'type': 'domain'}, {'type': 'domain'}]],
-                        subplot_titles=['UC 1', 'UC 2'])
-    fig.add_trace(go.Pie(labels=labels, values=values1, scalegroup='one',
-                         name="UC 1"), 1, 1)
-    fig.add_trace(go.Pie(labels=labels, values=values2, scalegroup='one',
-                         name="UC 2"), 1, 2)
+def create_trace3():
+    labels = ['thanks', 'shipping', 'handover', "silence", "other", "agree"]
+    trace_1 = go.Pie(labels=labels, values=[0, 2, 5, 7, 3, 1], scalegroup='one',
+                     name="UC1", direction="clockwise", sort=False, rotation=120, hoverinfo='label+percent',
+                     textinfo='label+value', textfont_size=15,
+                     marker=dict(line=dict(color='#000000', width=2)))
 
-    fig.update_layout(title_text='UC outcomes')
-    return fig
+    third_pie = html.Div(
+        # id="leads_source_container",
+        className="six columns chart_div pretty_container",
+        children=[
+            html.P("Third pie"),
+            dcc.Graph(
+                figure={"data": [trace_1]},
+                style={"height": "90%", "width": "98%"},
+                config=dict(displayModeBar=False),
+            ),
+        ],
+    ),
+    return third_pie
+
+
+def create_trace4():
+    labels = ['thanks', 'shipping', 'handover', "silence", "other", "agree"]
+    trace_2 = go.Pie(labels=labels, values=[0, 3, 13, 17, 5, 0], scalegroup='one',
+                     name="UC2", direction="clockwise", sort=False, rotation=120, hoverinfo='label+percent',
+                     textinfo='label+value', textfont_size=15,
+                     marker=dict(line=dict(color='#000000', width=2)))
+    forth_pie = html.Div(
+        # id="leads_source_container",
+        className="six columns chart_div pretty_container",
+        children=[
+            html.P("Forth pie"),
+            dcc.Graph(
+                figure={"data": [trace_2]},
+                style={"height": "90%", "width": "98%"},
+                config=dict(displayModeBar=False),
+            ),
+        ],
+    ),
+    return forth_pie
 
 
 def parse_contents(contents, filename, date):
@@ -99,50 +157,42 @@ def parse_contents(contents, filename, date):
             'There was an error processing this file.'
         ])
 
-    uc_proportion_in_month_fig = uc_proportion_in_month(["UC 1", "UC 2", "Other"], [18, 38, 167])
-    outcome_proportion_of_uc_fig = outcome_proportion_of_uc(
-        ["thanks", "shipping", "handover", "silence", "other", "agree"],
-        [0, 5, 18, 24, 8, 1])
-    outcome_of_uc_fig = outcome_of_uc(
-        ["thanks", "shipping", "handover", "silence", "other", "agree"],
-        [0, 2, 5, 7, 3, 1],
-        [0, 3, 13, 17, 5, 0],
-    )
-    return uc_proportion_in_month_fig, outcome_proportion_of_uc_fig, outcome_of_uc_fig
-    # return html.Div([
-    #     html.H5(filename),
-    #     html.H6(datetime.datetime.fromtimestamp(date)),
-    #
-    #     dash_table.DataTable(
-    #         data=df.to_dict('records'),
-    #         columns=[{'name': i, 'id': i} for i in df.columns]
-    #     ),
-    #
-    #     html.Hr(),  # horizontal line
-    #
-    #     # For debugging, display the raw contents provided by the web browser
-    #     html.Div('Raw Content'),
-    #     html.Pre(contents[0:200] + '...', style={
-    #         'whiteSpace': 'pre-wrap',
-    #         'wordBreak': 'break-all'
-    #     })
-    # ])
+    first_pie = create_trace1()
+    second_pie = create_trace2()
+    third_pie = create_trace3()
+
+    return first_pie
 
 
-@app.callback([Output('proportion-in-month', 'figure'),
-               Output('outcome-proportion-of-all-uc', 'figure'),
-               Output('outcome-of-uc', 'figure'), ],
-              [Input('upload-data', 'contents')],
-              [State('upload-data', 'filename'),
-               State('upload-data', 'last_modified')])
+@app.callback(
+    [
+        Output('first-pie', 'children'),
+        Output('second-pie', 'children'),
+        Output('third-pie', 'children'),
+        Output('forth-pie', 'children'),
+    ],
+    [
+        Input('upload-data', 'contents')
+    ],
+    [
+        State('upload-data', 'filename'),
+        State('upload-data', 'last_modified')
+    ])
 def update_output(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
-        # children = [
-        #     parse_contents(c, n, d) for c, n, d in
-        #     zip(list_of_contents, list_of_names, list_of_dates)]
-        for c, n, d in zip(list_of_contents, list_of_names, list_of_dates):
-            uc_proportion_in_month_fig, outcome_proportion_of_uc_fig, outcome_of_uc_fig = parse_contents(c, n, d)
-        return uc_proportion_in_month_fig, outcome_proportion_of_uc_fig, outcome_of_uc_fig
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        first_pie = create_trace1()
+        second_pie = create_trace2()
+        third_pie = create_trace3()
+        forth_pie = create_trace4()
+
+        # return first_pie, second_pie, third_pie
+        # return children[0]
+        return first_pie, second_pie, third_pie,forth_pie
+    else:
+        return "", "", "", ""
 
 
 if __name__ == '__main__':
