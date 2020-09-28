41937970b0cc058cdbba4b51d3e3dba5c0716653
DucAnh <ngducanh1223@gmail.com>
2020-09-28 18:07:27 +0700

    First part content and date picker

--- app_2.py
+++ app_2.py
@@ -17,8 +17,10 @@ from datetime import date, timedelta
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__,
+                external_stylesheets=[dbc.themes.BOOTSTRAP]
+                )
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.DataFrame({
@@ -44,16 +46,179 @@ app.layout = html.Div(children=[
             html.A(id="a4_title_sidebar", className="title_sidebar", children=["Outcome of each Use case"]),
         ]
     ),
-    # html.H1(children='Hello Dash'),
-    #
-    # html.Div(children='''
-    #     Dash: A web application framework for Python.
-    # '''),
-    #
-    # dcc.Graph(
-    #     id='example-graph',
-    #     figure=fig
-    # )
+
+    html.Div(
+        id="page_content_and_graph",
+        children=[
+            html.Div(
+                id="part_1_title",
+                children=["Overall Performance"],
+            ),
+            html.Div(
+                id="part_1_subtitle_1",
+                className="part_1_subtitle",
+                children=["Statistics by last week: abc"],
+            ),
+            html.Div(
+                className="basic-metrics",
+                children=[
+                    html.Div(
+                        className="col-md-4",
+                        children=[
+                            html.Div(
+                                id="no_conversations_lastweek",
+                                className="sub_basic_metrics",
+                                children=[
+                                    html.Div(
+                                        className="col-md-12",
+                                        children=[
+                                            html.Img(src="assets/icon/conversation_icon.png",
+                                                     className="sub_basic_metrics_img"),
+                                            html.P("Conversations", style={"display": "inline", "paddingLeft": "19px",
+                                                                           "fontSize": "19px"}),
+                                        ]
+                                    )
+                                ]
+                            ),
+                        ]
+                    ),
+                    html.Div(
+                        className="col-md-4",
+                        children=[
+                            html.Div(
+                                id="no_users_lastweek",
+                                className="sub_basic_metrics",
+                                children=[
+                                    html.Div(
+                                        className="col-md-12",
+                                        children=[
+                                            html.Img(src="assets/icon/user_icon.png",
+                                                     className="sub_basic_metrics_img"),
+                                            html.P("Users", style={"display": "inline", "paddingLeft": "19px",
+                                                                   "fontSize": "19px"}),
+                                        ]
+                                    )
+                                ]
+                            ),
+                        ]
+                    ),
+                    html.Div(
+                        className="col-md-4",
+                        children=[
+                            html.Div(
+                                id="success_rate_lastweek",
+                                className="sub_basic_metrics",
+                                children=[
+                                    html.Div(
+                                        className="col-md-12",
+                                        children=[
+                                            html.Img(src="assets/icon/success_rate_icon.png",
+                                                     className="sub_basic_metrics_img"),
+                                            html.P("Success Rate", style={"display": "inline", "paddingLeft": "19px",
+                                                                          "fontSize": "19px"}),
+                                        ]
+                                    )
+                                ]
+                            ),
+                        ]
+                    ),
+                ]
+            ),
+            html.Div(
+                id="part_1_subtitle_2",
+                className="part_1_subtitle",
+                children=["Statistics by month"],
+            ),
+            html.Div(
+                className="basic-metrics",
+                children=[
+                    html.Div(
+                        className="col-md-4",
+                        children=[
+                            html.Div(
+                                id="no_conversations_by_month",
+                                className="sub_basic_metrics",
+                                children=[
+                                    html.Div(
+                                        className="col-md-12",
+                                        children=[
+                                            # html.Img(src="assets/icon/conversation_icon.png", className="sub_basic_metrics_img"),
+                                            html.P("Conversations by month",
+                                                   style={"display": "inline", "paddingLeft": "19px",
+                                                          "fontSize": "19px"}),
+                                        ]
+                                    )
+                                ]
+                            ),
+                        ]
+                    ),
+                    html.Div(
+                        className="col-md-4",
+                        children=[
+                            html.Div(
+                                id="no_users_by_month",
+                                className="sub_basic_metrics",
+                                children=[
+                                    html.Div(
+                                        className="col-md-12",
+                                        children=[
+                                            html.P("Users by month", style={"display": "inline", "paddingLeft": "19px",
+                                                                            "fontSize": "19px"}),
+                                        ]
+                                    )
+                                ]
+                            ),
+                        ]
+                    ),
+                    html.Div(
+                        className="col-md-4",
+                        children=[
+                            html.Div(
+                                id="success_rate_by_month",
+                                className="sub_basic_metrics",
+                                children=[
+                                    html.Div(
+                                        className="col-md-12",
+                                        children=[
+                                            html.P("Success rate by month",
+                                                   style={"display": "inline", "paddingLeft": "19px",
+                                                          "fontSize": "19px"}),
+                                        ]
+                                    )
+                                ]
+                            ),
+                        ]
+                    ),
+                ]
+            ),
+            html.Div(
+                id="part_1_subtitle_3",
+                className="part_1_subtitle",
+                children=["Overall Performance by selected period"],
+            ),
+            html.Div(
+                children=[
+                    dcc.DatePickerSingle(
+                        id='my_date_picker_start',
+                        min_date_allowed=dt(2020, 1, 1),
+                        max_date_allowed=dt(2021, 12, 31),
+                        style={"borderRadius": "25px"},
+                        placeholder='Start date',
+                    ),
+                    dcc.DatePickerSingle(
+                        style={"paddingLeft": "20px", "borderRadius": "25px", "paddingRight": "20px"},
+                        id='my_date_picker_end',
+                        min_date_allowed=dt(2020, 1, 1),
+                        max_date_allowed=dt(2021, 12, 31),
+                        placeholder='End date',
+                    ),
+                    html.Button('Run', id='run-analytics', style={"borderRadius": "4px", "backgroundColor": "#448efc", "color": "white"}, ),
+                ]
+            )
+
+        ],
+    ),
+
 ])
 
 if __name__ == '__main__':
