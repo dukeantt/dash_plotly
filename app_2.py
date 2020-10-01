5c7779abb2298ded4d15cc935b100a183eb2b60c
DucAnh <ngducanh1223@gmail.com>
2020-10-01 16:59:11 +0700

    Handle date picker and run button, show basic metrics in period when press button

--- app_2.py
+++ app_2.py
@@ -22,6 +22,9 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__,
                 external_stylesheets=[dbc.themes.BOOTSTRAP]
                 )
+metrics_in_period_text_style = {"display": "block", "paddingLeft": "7rem",
+                                "fontSize": "27px", "fontWeight": "bold",
+                                "marginTop": "-1rem"}
 
 today = datetime.date.today()
 monday = today - datetime.timedelta(days=today.weekday())
@@ -261,6 +264,7 @@ app.layout = html.Div(children=[
                         id='my_date_picker_start',
                         min_date_allowed=dt(2020, 1, 1),
                         max_date_allowed=dt(2021, 12, 31),
+                        display_format='D/M/Y',
                         placeholder='Start date',
                     ),
                     dcc.DatePickerSingle(
@@ -268,6 +272,7 @@ app.layout = html.Div(children=[
                         id='my_date_picker_end',
                         min_date_allowed=dt(2020, 1, 1),
                         max_date_allowed=dt(2021, 12, 31),
+                        display_format='D/M/Y',
                         placeholder='End date',
                     ),
                     html.Button('Run', id='run-analytics',
@@ -292,10 +297,7 @@ app.layout = html.Div(children=[
                                                      className="sub_basic_metrics_img"),
                                             html.P("Conversations", style={"display": "block", "paddingLeft": "7rem",
                                                                            "fontSize": "19px", "marginTop": "-4rem"}),
-                                            html.P("'",
-                                                   style={"display": "block", "paddingLeft": "7rem",
-                                                          "fontSize": "27px", "fontWeight": "bold",
-                                                          "marginTop": "-1rem"}),
+                                            html.Div(id="no_conversations_in_period_text",),
                                         ]
                                     )
                                 ]
@@ -313,13 +315,11 @@ app.layout = html.Div(children=[
                                         className="col-md-12",
                                         children=[
                                             html.Img(src="assets/icon/user_icon.png",
-                                                     className="sub_basic_metrics_img"),
+                                                     className="sub_basic_metrics_img",
+                                                     style={"padding-top": "2.5rem"}),
                                             html.P("Users", style={"display": "block", "paddingLeft": "7rem",
                                                                    "fontSize": "19px", "marginTop": "-4rem"}),
-                                            html.P("'",
-                                                   style={"display": "block", "paddingLeft": "7rem",
-                                                          "fontSize": "27px", "fontWeight": "bold",
-                                                          "marginTop": "-1rem"}),
+                                            html.Div(id="no_users_in_period_text",),
                                         ]
                                     )
                                 ]
@@ -340,10 +340,7 @@ app.layout = html.Div(children=[
                                                      className="sub_basic_metrics_img"),
                                             html.P("Success Rate", style={"display": "block", "paddingLeft": "7rem",
                                                                           "fontSize": "19px", "marginTop": "-4rem"}),
-                                            html.P("'",
-                                                   style={"display": "block", "paddingLeft": "7rem",
-                                                          "fontSize": "27px", "fontWeight": "bold",
-                                                          "marginTop": "-1rem"}),
+                                            html.Div(id="success_rate_in_period_text",),
                                         ]
                                     )
                                 ]
@@ -840,6 +837,11 @@ app.layout = html.Div(children=[
                 ]
             ),
 
+            html.Div(id='df-data', style={'display': 'none'}),
+            html.P(id='custom_from_date_data', style={'display': 'none'}),
+            html.P(id='custom_to_date_data', style={'display': 'none'}),
+            html.P(id='is-click', style={'display': 'none'}, children=["0"]),
+
             html.Div(
                 id="space_at_the_end",
                 style={"paddingBottom": "100px"},
@@ -848,6 +850,134 @@ app.layout = html.Div(children=[
     ),
 
 ])
+
+
+# Xu ly kho chon ngay
+@app.callback(
+    [
+        Output(component_id='custom_from_date_data', component_property='children'),
+        Output(component_id='custom_to_date_data', component_property='children'),
+
+    ],
+    [
+        Input('my_date_picker_start', 'date'),
+        Input('my_date_picker_end', 'date'),
+    ],
+)
+def set_date_data(start_date, end_date):
+    if start_date is not None and end_date is not None:
+        return start_date, end_date
+    else:
+        return "", ""
+
+
+# Xu ly khi an button Run
+@app.callback(
+    # [
+    # Output(component_id='loading-div', component_property='children'),
+    # Output(component_id='loading-div-2', component_property='children'),
+    # Output(component_id='is-click', component_property='children'),
+    # ],
+    Output(component_id='is-click', component_property='children'),
+
+    [
+        Input(component_id='run-analytics', component_property='n_clicks')
+    ],
+    [
+        State('custom_from_date_data', 'children'),
+        State('custom_to_date_data', 'children'),
+        # State('loading-div', 'style'),
+        # State('loading-div-2', 'style'),
+    ]
+)
+def show_loading(n_clicks, start_date, end_date):
+    if start_date is not None and end_date is not None:
+        # spinner = randrange(2)
+        # loading_child = html.Div(style={
+        #     'display': 'block',
+        #     'position': 'fixed',
+        #     'top': '0',
+        #     'bottom': '0',
+        #     'left': '-200',
+        #     'right': '-100',
+        #     'backgroundColor': '#f9f9f9',
+        #     'opacity': '0.8',
+        #     'zIndex': '1002',
+        #     'width': '2000px',
+        # },
+        #     children=[html.Img(id="cat_loading_spinner", src=spinner_list[spinner], style={"position": "sticky"})]
+        # )
+        # display_loading_1 = loading1["display"]
+        # display_loading_2 = loading2["display"]
+        # if display_loading_1 != "none":
+        #     loading_1_child = loading_child
+        #     loading_2_child = ""
+        #     return loading_1_child, loading_2_child, "1"
+        # elif display_loading_2 != "none":
+        #     loading_1_child = ""
+        #     loading_2_child = loading_child
+        #     return loading_1_child, loading_2_child, "1"
+        return "1"
+    else:
+        # return html.Div(style={'display': 'none'}), html.Div(style={'display': 'none'}), "0"
+        return "0"
+
+
+@app.callback(
+    Output('df-data', 'children'),
+    [
+        Input('is-click', 'children'),
+    ],
+    [
+        State('custom_from_date_data', 'children'),
+        State('custom_to_date_data', 'children'),
+    ],
+)
+def handle_df(is_click, start_date, end_date):
+    if is_click == "1" and start_date is not None and end_date is not None:
+        df = get_data_from_table("conversation_outcome", from_date=start_date, to_date=end_date)
+        if len(df) == 0:
+            return None
+        df = df.drop(columns=["_id"])
+        return df.to_json(date_format='iso', orient='split')
+    else:
+        return None
+
+
+@app.callback(
+    [
+        Output("no_conversations_in_period_text", 'children'),
+        Output("no_users_in_period_text", 'children'),
+        Output("success_rate_in_period_text", 'children'),
+
+    ],
+
+    [
+        Input('df-data', 'children')
+    ],
+    # [
+    #     State('loading-div', 'style'),
+    #     State('loading-div-2', 'style')
+    # ]
+)
+def update_output(df):
+    if df is not None:
+        df = pd.read_json(df, orient="split")
+
+        no_conversations_in_period = get_number_of_conversation(df)
+        # no_users_in_period = get_number_of_user(df)
+        success_rate_in_period = get_success_rate(df)
+
+        no_conversations_in_period_text = html.P(str(no_conversations_in_period), style=metrics_in_period_text_style)
+        no_users_in_period_text = html.P("2", style=metrics_in_period_text_style)
+        success_rate_in_period_text = html.P(str(success_rate_in_period), style=metrics_in_period_text_style)
+        return no_conversations_in_period_text, no_users_in_period_text, success_rate_in_period_text
+    else:
+        no_conversations_in_period_text = html.P("'", style=metrics_in_period_text_style)
+        no_users_in_period_text = html.P("'", style=metrics_in_period_text_style)
+        success_rate_in_period_text = html.P("'", style=metrics_in_period_text_style)
+        return no_conversations_in_period_text, no_users_in_period_text, success_rate_in_period_text
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
